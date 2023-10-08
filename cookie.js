10ab449c933ecd5f188a4b7905a16e68134c3b0e
SulmanOlieko <141807255+SulmanOlieko@users.noreply.github.com>
2023-10-08 22:11:31 +0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,14 +1,19 @@
-// Function to check if the "cookieConsent" cookie exists
-function checkCookie() {
-    return document.cookie.split('; ').some((item) => item.startsWith('cookieConsent='));
-}
-
 // Function to set a cookie with a specific expiration time
 function setCookie(name, value, minutes) {
     const date = new Date();
     date.setTime(date.getTime() + (minutes * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
     document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Function to check if the "cookieConsent" cookie exists and has expired
+function isCookieExpired() {
+    const cookie = document.cookie.split('; ').find((item) => item.startsWith('cookieConsent='));
+    if (cookie) {
+        const expirationTime = new Date(cookie.replace(/(?:(?:^|.*;\s*)cookieConsent\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
+        return expirationTime < new Date();
+    }
+    return false;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -31,15 +36,10 @@ document.addEventListener("DOMContentLoaded", function () {
         setCookie("cookieConsent", "declined", 60); // Cookie consent declined for 1 hour
     });
 
-    // Check if the user has previously accepted or declined cookies
-    if (!checkCookie()) {
+    // Check if the user has previously accepted or declined cookies and if the consent has expired
+    if (!isCookieExpired() && (document.cookie.indexOf("cookieConsent=accepted") !== -1 || document.cookie.indexOf("cookieConsent=declined") !== -1)) {
+        cookieBanner.style.display = "none";
+    } else {
         showCookieBanner();
-    } else if (document.cookie.indexOf("cookieConsent=declined") !== -1) {
-        // If the user previously declined cookies, and it's been more than 1 hour, show the banner again.
-        const now = new Date().getTime();
-        const lastDeclineTime = new Date(document.cookie.replace(/(?:(?:^|.*;\s*)lastDeclineTime\s*\=\s*([^;]*).*$)|^.*$/, "$1")).getTime();
-        if (now - lastDeclineTime >= 60 * 60 * 1000) {
-            showCookieBanner();
-        }
     }
 });
