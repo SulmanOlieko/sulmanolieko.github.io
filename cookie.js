2f0d3ff63e2a11173bf6f80f2f70b474c65537bf
SulmanOlieko <141807255+SulmanOlieko@users.noreply.github.com>
2023-10-08 21:58:38 +0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -3,10 +3,10 @@ function checkCookie() {
     return document.cookie.split('; ').some((item) => item.startsWith('cookieConsent='));
 }
 
-// Function to set a cookie
-function setCookie(name, value, days) {
+// Function to set a cookie with a specific expiration time
+function setCookie(name, value, minutes) {
     const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
     document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
@@ -16,18 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const acceptCookies = document.getElementById("accept-cookies");
     const declineCookies = document.getElementById("decline-cookies");
 
+    // Function to display the cookie banner
+    function showCookieBanner() {
+        cookieBanner.style.display = "block";
+    }
+
     acceptCookies.addEventListener("click", function () {
         cookieBanner.style.display = "none";
-        setCookie("cookieConsent", "accepted", 7); // Cookie consent accepted for 7 days
+        setCookie("cookieConsent", "accepted", 7 * 24 * 60); // Cookie consent accepted for 7 days
     });
 
     declineCookies.addEventListener("click", function () {
         cookieBanner.style.display = "none";
-        setCookie("cookieConsent", "declined", 7); // Cookie consent declined for 7 days
+        setCookie("cookieConsent", "declined", 60); // Cookie consent declined for 1 hour
     });
 
     // Check if the user has previously accepted or declined cookies or if they have cleared their cookies
     if (!checkCookie()) {
-        cookieBanner.style.display = "block";
+        showCookieBanner();
     }
 });
