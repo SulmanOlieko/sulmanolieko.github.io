eed9cede88ee280abee3b2751bde59ecf1b0d82c
SulmanOlieko <141807255+SulmanOlieko@users.noreply.github.com>
2023-10-14 20:30:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ const cookieBox = document.querySelector(".wrapper"),
   buttons = document.querySelectorAll(".button");
 
 const executeCodes = () => {
-  //if cookie contains codinglab it will be returned and below of this code will not run
-  if (document.cookie.includes("codinglab")) return;
-  cookieBox.classList.add("show");
+  // Check if the user has already accepted the cookie
+  if (document.cookie.includes("cookieBy=codinglab")) {
+    cookieBox.style.display = "none"; // Hide the cookie box
+    return;
+  }
+
+  cookieBox.style.display = "block"; // Display the cookie box if it wasn't accepted
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      cookieBox.classList.remove("show");
+      // Hide the cookie box when the user accepts the cookie
+      cookieBox.style.display = "none";
 
       //if button has acceptBtn id
       if (button.id == "acceptBtn") {
-        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-        document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
+        // Set cookies for 1 month. 60 = 1 min, 60 = 1 hour, 24 = 1 day, 30 = 30 days
+        document.cookie = "cookieBy=codinglab; max-age=" + 60 * 60 * 24 * 30;
       }
     });
   });
 };
 
-//executeCodes function will be called on webpage load
+// Execute the code when the page is loaded
 window.addEventListener("load", executeCodes);
